bd3f91c6889c57ef23d33ed5b198c19ad1047116
LiuYang <fyeeme@gmail.com>
2022-05-02 21:24:39 +0800

    feat: spring cloud gateway add filter

---
+++ api-gateway/src/main/java/com/fyeeme/ApiConfig.java
@@ -0,0 +1,35 @@
+package com.fyeeme;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class ApiConfig {
+    public static final Logger log = LoggerFactory.getLogger(ApiConfig.class);
+
+    @Bean
+    public GlobalFilter customFilter() {
+        return new CustomGlobalFilter();
+    }
+
+    public static class CustomGlobalFilter implements GlobalFilter, Ordered {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            log.info("custom global filter");
+            return chain.filter(exchange);
+        }
+
+        @Override
+        public int getOrder() {
+            return -1;
+        }
+    }
+}
