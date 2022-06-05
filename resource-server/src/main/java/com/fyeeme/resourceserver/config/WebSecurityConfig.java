86eb0734698985d9fcd70f8ad64259b8d23dae37
LiuYang <fyeeme@gmail.com>
2022-06-05 14:24:12 +0800

    feat: add cors based on spring security

--- resource-server/src/main/java/com/fyeeme/resourceserver/config/WebSecurityConfig.java
+++ resource-server/src/main/java/com/fyeeme/resourceserver/config/WebSecurityConfig.java
@@ -1,21 +1,53 @@
 package com.fyeeme.resourceserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.mvcMatcher("/messages/**")
-                .authorizeRequests()
-                .mvcMatchers("/messages/**").access("hasAuthority('SCOPE_message.read')")
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+        http
+                .mvcMatcher("/messages/**")
+                .authorizeHttpRequests((authorize) -> authorize
+                        .antMatchers(HttpMethod.GET, "/message/**").hasAuthority("SCOPE_message:read")
+                        .antMatchers(HttpMethod.POST, "/message/**").hasAuthority("SCOPE_message:write")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
+
+    //The easiest way to ensure that CORS is handled first is to use the CorsWebFilter. Users can integrate the CorsWebFilter
+    // with Spring Security by providing a CorsConfigurationSource.
+    // For example, the following will integrate CORS support within Spring Security:
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:3000"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+//
+//        cc.setAllowCredentials(true);
+//        cc.setAllowedOrigins(List.of("http://127.0.0.1:3000"));
+//        cc.setAllowedHeaders(List.of("*"));
+//        cc.setAllowedMethods(List.of("*"));
+    }
+
 }
